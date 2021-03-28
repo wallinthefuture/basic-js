95cf3c3c274bf2b3519c964c9cc7a37bbe171626
wallinthefuture <wallinthefuture@mail.ru>
2021-03-28 12:39:21 +0300

    feat: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (newArr.length !== 0 && arr[i - 2] !== '--discard-next') {
+        newArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0 && arr[i - 2] !== '--discard-next') {
+        newArr.push(arr[i - 1]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr.filter((el) => el !== undefined);
 };
